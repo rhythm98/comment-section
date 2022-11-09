7abd25217f613f98307d3e5849387acd3854b470
rhythm98 <rhyagr@gmail.com>
2022-11-09 21:08:22 +0530

    assignment

---
+++ README.md
@@ -0,0 +1,18 @@
+## Software Engineer Task
+
+### Problem Statement
+
+> Build an interactive comments section similar to the one below  
+![Alt text](sample.jpeg "sample image")  
+## Requirements
+
+1. The comments section should be responsive and have optimal layout on all screen sizes.
+2. A user should be able to read, post, edit & delete comments.
+3. A user can reply to a comment, and a reply should be nested inside its parent comment.
+4. A user should be able to upvote a comment.
+
+**Note 1:** You’re free to choose any framework/library to build this application. You can also choose to build it as a frontend-only or a full stack application.
+
+**Note 2:** We’ll be testing the app for both UI & functionality. So, please ensure that the UI is accurate & looks pleasing.
+
+### Live Site URL: [Github Pages](https://rhythm98.github.io/comment-section/)
---
+++ app.js
@@ -0,0 +1,229 @@
+const data = {
+  currentUser: {
+    image: {
+      webp: './images/avatars/50.png'
+    },
+    username: 'Ramesh',
+  },
+  comments: [
+    {
+      parent: 0,
+      id: 1,
+      content:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. ",
+      createdAt: '1 month ago',
+      score: 12,
+      user: {
+        image: {
+          webp: './images/avatars/10.png'
+        },
+        username: 'Suresh',
+      },
+      replies: [],
+    },
+    {
+      parent: 0,
+      id: 2,
+      content:
+        'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.',
+      createdAt: '2 weeks ago',
+      score: 5,
+      user: {
+        image: {
+          webp: './images/avatars/30.png'
+        },
+        username: 'Ghanshyam',
+      },
+      replies: [
+        {
+          parent: 2,
+          id: 1,
+          content:
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ",
+          createdAt: '1 week ago',
+          score: 4,
+          replyingTo: 'Ghanshyam',
+          user: {
+            image: {
+              webp: './images/avatars/40.png'
+            },
+            username: 'Babloo',
+          },
+        },
+        {
+          parent: 2,
+          id: 1,
+          content:
+            'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin ',
+          createdAt: '2 days ago',
+          score: 2,
+          replyingTo: 'Babloo',
+          user: {
+            image: {
+              webp: './images/avatars/50.png'
+            },
+            username: 'Dholu',
+          },
+        },
+      ],
+    },
+  ],
+}
+function appendFrag(frag, parent) {
+  var children = [].slice.call(frag.childNodes, 0);
+  parent.appendChild(frag);
+  //console.log(children);
+  return children[1];
+}
+
+const addComment = (body, parentId, replyTo = undefined) => {
+  let commentParent =
+    parentId === 0
+      ? data.comments
+      : data.comments.filter((c) => c.id == parentId)[0].replies;
+  let newComment = {
+    parent: parentId,
+    id:
+      commentParent.length == 0
+        ? 1
+        : commentParent[commentParent.length - 1].id + 1,
+    content: body,
+    createdAt: "Now",
+    replyingTo: replyTo,
+    score: 0,
+    replies: parent == 0 ? [] : undefined,
+    user: data.currentUser,
+  };
+  commentParent.push(newComment);
+  initComments();
+};
+const deleteComment = (commentObject) => {
+  if (commentObject.parent == 0) {
+    data.comments = data.comments.filter((e) => e != commentObject);
+  } else {
+    data.comments.filter((e) => e.id === commentObject.parent)[0].replies =
+      data.comments
+        .filter((e) => e.id === commentObject.parent)[0]
+        .replies.filter((e) => e != commentObject);
+  }
+  initComments();
+};
+
+const promptDel = (commentObject) => {
+  const modalWrp = document.querySelector(".modal-wrp");
+  modalWrp.classList.remove("invisible");
+  modalWrp.querySelector(".yes").addEventListener("click", () => {
+    deleteComment(commentObject);
+    modalWrp.classList.add("invisible");
+  });
+  modalWrp.querySelector(".no").addEventListener("click", () => {
+    modalWrp.classList.add("invisible");
+  });
+};
+
+const spawnReplyInput = (parent, parentId, replyTo = undefined) => {
+  if (parent.querySelectorAll(".reply-input")) {
+    parent.querySelectorAll(".reply-input").forEach((e) => {
+      e.remove();
+    });
+  }
+  const inputTemplate = document.querySelector(".reply-input-template");
+  const inputNode = inputTemplate.content.cloneNode(true);
+  const addedInput = appendFrag(inputNode, parent);
+  addedInput.querySelector(".bu-primary").addEventListener("click", () => {
+    let commentBody = addedInput.querySelector(".cmnt-input").value;
+    if (commentBody.length == 0) return;
+    addComment(commentBody, parentId, replyTo);
+  });
+};
+
+const createCommentNode = (commentObject) => {
+  const commentTemplate = document.querySelector(".comment-template");
+  var commentNode = commentTemplate.content.cloneNode(true);
+  commentNode.querySelector(".usr-name").textContent =
+    commentObject.user.username;
+  commentNode.querySelector(".usr-img").src = commentObject.user.image.webp;
+  commentNode.querySelector(".score-number").textContent = commentObject.score;
+  commentNode.querySelector(".cmnt-at").textContent = commentObject.createdAt;
+  commentNode.querySelector(".c-body").textContent = commentObject.content;
+  if (commentObject.replyingTo)
+    commentNode.querySelector(".reply-to").textContent =
+      "@" + commentObject.replyingTo;
+
+  commentNode.querySelector(".score-plus").addEventListener("click", () => {
+    commentObject.score++;
+    initComments();
+  });
+
+  commentNode.querySelector(".score-minus").addEventListener("click", () => {
+    commentObject.score--;
+    if (commentObject.score < 0) commentObject.score = 0;
+    initComments();
+  });
+  if (commentObject.user.username == data.currentUser.username) {
+    commentNode.querySelector(".comment").classList.add("this-user");
+    commentNode.querySelector(".delete").addEventListener("click", () => {
+      promptDel(commentObject);
+    });
+    commentNode.querySelector(".edit").addEventListener("click", (e) => {
+      const path = e.path[3].querySelector(".c-body");
+      if (
+        path.getAttribute("contenteditable") == false ||
+        path.getAttribute("contenteditable") == null
+      ) {
+        path.setAttribute("contenteditable", true);
+        path.focus()
+      } else {
+        path.removeAttribute("contenteditable");
+      }
+      
+    });
+    return commentNode;
+  }
+  return commentNode;
+};
+
+const appendComment = (parentNode, commentNode, parentId) => {
+  const bu_reply = commentNode.querySelector(".reply");
+  // parentNode.appendChild(commentNode);
+  const appendedCmnt = appendFrag(commentNode, parentNode);
+  const replyTo = appendedCmnt.querySelector(".usr-name").textContent;
+  bu_reply.addEventListener("click", () => {
+    if (parentNode.classList.contains("replies")) {
+      spawnReplyInput(parentNode, parentId, replyTo);
+    } else {
+      //console.log(appendedCmnt.querySelector(".replies"));
+      spawnReplyInput(
+        appendedCmnt.querySelector(".replies"),
+        parentId,
+        replyTo
+      );
+    }
+  });
+};
+
+function initComments(
+  commentList = data.comments,
+  parent = document.querySelector(".comments-wrp")
+) {
+  parent.innerHTML = "";
+  commentList.forEach((element) => {
+    var parentId = element.parent == 0 ? element.id : element.parent;
+    const comment_node = createCommentNode(element);
+    if (element.replies && element.replies.length > 0) {
+      initComments(element.replies, comment_node.querySelector(".replies"));
+    }
+    appendComment(parent, comment_node, parentId);
+  });
+}
+
+initComments();
+const cmntInput = document.querySelector(".reply-input");
+cmntInput.querySelector(".bu-primary").addEventListener("click", () => {
+  let commentBody = cmntInput.querySelector(".cmnt-input").value;
+  if (commentBody.length == 0) return;
+  addComment(commentBody, 0);
+  cmntInput.querySelector(".cmnt-input").value = "";
+});
+
+// addComment("Hello ! It works !!",0);
